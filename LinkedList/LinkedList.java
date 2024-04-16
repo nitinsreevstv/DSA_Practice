4a5a2290a7fc0686c72a951329a2946a55b7ad55
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-04-17 01:19:10 +0530

    Added comment

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -46,6 +46,8 @@ public class LinkedList {
 		System.out.println();
 	}
 	
+	// to add node at any index
+	
 	public void add(int idx, int value) {
 		Node newNode = new Node(value);
 		Node temp = head;
